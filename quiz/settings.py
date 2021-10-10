cd7fa3c123128abbff3dd0c871d27067e1121617
sandra fish <sandrafish@comcast.net>
2021-10-10 12:12:00 -0600

    static and key

--- quiz/settings.py
+++ quiz/settings.py
@@ -120,8 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #'/static/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
